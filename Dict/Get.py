e65522a223d210791a7158fdb9f8029284729677
Rick Graves <gravesricharde@yahoo.com>
2018-01-28 08:36:14 -0800

    for OrderedDict 2/3 compatibility, import get_ident from six.moves._dummy_thread

--- Dict/Get.py
+++ Dict/Get.py
@@ -331,10 +331,7 @@ def getAnyValue( d ):
 # Backport of OrderedDict() class that runs on Python 2.4, 2.5, 2.6, 2.7 and pypy.
 # Passes Python2.7's test suite and incorporates all the latest updates.
 
-try:
-    from thread import get_ident as _get_ident
-except ImportError:
-    from dummy_thread import get_ident as _get_ident
+from six.moves._dummy_thread import get_ident as _get_ident
 
 try:
     from _abcoll import KeysView, ValuesView, ItemsView
