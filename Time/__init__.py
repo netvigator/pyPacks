e767b7d83c188240de9a10c6a3cb981e92c1f334
Rick Graves <gravesricharde@yahoo.com>
2018-02-10 06:55:06 +0700

    put all time constants here, more DRY, fewer circular import problems

--- Time/__init__.py
+++ Time/__init__.py
@@ -3,6 +3,15 @@
 
 iSecsPerDay = 24 * 3600
 
-sFormatISO  = '%Y-%m-%d %H:%M:%S'
+sFormatISOdateTime          = '%Y-%m-%d %H:%M:%S'
+sFormatISOdate              = '%Y-%m-%d'
+sFormatIsoNoSecs            = '%Y-%m-%d %H:%M'
+sFormatDateAm               = '%m/%d/%Y'
+sFormatDateAmShort          = '%m/%d/%y'
+sFormatDateEu               = '%d/%m/%Y'
+sFormatISONoSpace           = '%Y-%m-%d_%H.%M.%S'
+sFormatISOdateTimeNoColon   = '%Y-%m-%d_%H.%M.%S'
+sFormatISOdateTimeNoSpace   = '%Y-%m-%d_%H:%M:%S'
 
-
+sFormatNatureUSA            = '%d %m %Y'
+sFormatUSAdateTime          = '%m/%d/%Y %I:%M:%S %p'
