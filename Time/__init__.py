90c9c3a50128eb7488f6ee42086ba57ce3fd6b18
Rick Graves <gravesricharde@yahoo.com>
2019-11-16 03:42:02 +0700

    all constant names here get an underscore in front, to improve readability of module imports

--- Time/__init__.py
+++ Time/__init__.py
@@ -1,17 +1,17 @@
 
 
 
-iSecsPerDay = 24 * 3600
+_iSecsPerDay = 24 * 3600
 
-sFormatISOdateTime          = '%Y-%m-%d %H:%M:%S'
-sFormatISOdate              = '%Y-%m-%d'
-sFormatIsoNoSecs            = '%Y-%m-%d %H:%M'
-sFormatDateAm               = '%m/%d/%Y'
-sFormatDateAmShort          = '%m/%d/%y'
-sFormatDateEu               = '%d/%m/%Y'
-sFormatISONoSpace           = '%Y-%m-%d_%H.%M.%S'
-sFormatISOdateTimeNoColon   = '%Y-%m-%d_%H.%M.%S'
-sFormatISOdateTimeNoSpace   = '%Y-%m-%d_%H:%M:%S'
+_sFormatISOdateTime         = '%Y-%m-%d %H:%M:%S'
+_sFormatISOdate             = '%Y-%m-%d'
+_sFormatIsoNoSecs           = '%Y-%m-%d %H:%M'
+_sFormatDateAm              = '%m/%d/%Y'
+_sFormatDateAmShort         = '%m/%d/%y'
+_sFormatDateEu              = '%d/%m/%Y'
+_sFormatISONoSpace          = '%Y-%m-%d_%H.%M.%S'
+_sFormatISOdateTimeNoColon  = '%Y-%m-%d_%H.%M.%S'
+_sFormatISOdateTimeNoSpace  = '%Y-%m-%d_%H:%M:%S'
 
-sFormatNatureUSA            = '%d %m %Y'
-sFormatUSAdateTime          = '%m/%d/%Y %I:%M:%S %p'
+_sFormatNatureUSA           = '%d %m %Y'
+_sFormatUSAdateTime         = '%m/%d/%Y %I:%M:%S %p'
