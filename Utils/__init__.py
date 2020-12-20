193398b3abb7a939d80cf0094fe66e11381bfca5
Rick Graves <gravesricharde@yahoo.com>
2020-12-20 19:42:30 +0700

    put in the current dump of the ebay markets table, for testing

--- Utils/__init__.py
+++ Utils/__init__.py
@@ -1,1 +1,28 @@
- 
+sMarketsTable = \
+'''
+  cMarket   | cCountry | cLanguage | iEbaySiteID | bHasCategories | iCategoryVer | cCurrencyDef | iUtcPlusOrMinus | cUseCategoryID
+------------+----------+-----------+-------------+----------------+--------------+--------------+-----------------+----------------
+ EBAY-US    | US       | en-US     |           0 | t              |          124 | USD          |              -8 |
+ EBAY-ENCA  | CA       | en-CA     |           2 | t              |          122 | CAD          |              -8 |
+ EBAY-GB    | GB       | en-GB     |           3 | t              |          115 | GBP          |               0 |
+ EBAY-AU    | AU       | en-AU     |          15 | t              |          116 | AUD          |              10 |
+ EBAY-AT    | AT       | de-AT     |          16 | t              |           93 | EUR          |               1 |
+ EBAY-FRBE  | BE       | fr-BE     |          23 | f              |           88 | EUR          |               1 |
+ EBAY-FR    | FR       | fr-FR     |          71 | t              |          112 | EUR          |               1 |
+ EBAY-DE    | DE       | de-DE     |          77 | t              |          125 | EUR          |               1 |
+ EBAY-MOTOR | US       | en-US     |         100 | t              |           75 | USD          |              -8 |
+ EBAY-IT    | IT       | it-IT     |         101 | t              |          104 | EUR          |               1 |
+ EBAY-NLBE  | BE       | nl-BE     |         123 | f              |           92 | EUR          |               1 |
+ EBAY-NL    | NL       | nl-NL     |         146 | t              |           86 | EUR          |               1 |
+ EBAY-ES    | ES       | es-ES     |         186 | t              |           89 | EUR          |               1 |
+ EBAY-CH    | CH       | de-CH     |         193 | t              |           94 | CHF          |               1 |
+ EBAY-HK    | HK       | zh-Hant   |         201 | t              |           27 | HKD          |               8 |
+ EBAY-IN    | IN       | en-IN     |         203 | t              |           65 | INR          |               5 |
+ EBAY-IE    | IE       | en-IE     |         205 | t              |           73 | EUR          |               1 |
+ EBAY-MY    | MY       | en-MY     |         207 | t              |           38 | MYR          |               8 |
+ EBAY-FRCA  | CA       | fr-CA     |         210 | f              |           75 | CAD          |              -8 |
+ EBAY-PH    | PH       | en-PH     |         211 | t              |           38 | PHP          |               8 |
+ EBAY-PL    | PL       | pl-PL     |         212 | t              |           57 | PLN          |               1 |
+ EBAY-SG    | SG       | en-SG     |         216 | t              |           37 | SGD          |               8 |
+ EBAY-SE    | SE       | sv-SE     |         218 | f              |           11 | SEK          |               1 |
+'''
